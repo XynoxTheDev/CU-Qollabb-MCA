c42835b380515ad18a99cb4d9dc9902b914561c2
Arindam Hazra <arindamhazra987@gmail.com>
2026-04-11 20:04:52 +0530

    fix: correct Fragment closing tag

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -277,7 +277,7 @@ export default function AdminOrdersPage() {
                           </TableCell>
                         </TableRow>
                       )}
-                    </React.Fragment>
+                    </Fragment>
                   ))}
                 </TableBody>
               </Table>
